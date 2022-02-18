57499cb60e58f17b8448d7baef99cfbccb6d39a1
svetlana-gor <susanna_7@mail.ru>
2022-02-18 15:39:27 +0200

    INTAP-1649: Add Name Parts block to User view page - added Training/UserNamingBundle - decorated EntityNameProvider service to render user name as "<LastName> <FirstName> <MiddleName>" - added the block to User view page to render full name and all name parts

---
+++ src/Training/Bundle/UserNamingBundle/DependencyInjection/UserNamingExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * UserNamingExtension class loads UserNamingBundle service configuration
+ */
+class UserNamingExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
---
+++ src/Training/Bundle/UserNamingBundle/UserNamingBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The UserNamingBundle bundle class.
+ */
+class UserNamingBundle extends Bundle
+{
+}
